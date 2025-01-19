6526509775279eea62f07b37619c54d298a2e818
betul171 <140914016+betul171@users.noreply.github.com>
2025-01-19 13:21:41 +0300

    Update zigzag_GNR.py

--- zigzag_GNR.py
+++ zigzag_GNR.py
@@ -25,7 +25,7 @@ variable    convert equal ${eV2J}*${eV2J}/${ps2s}/${A2m}
 dimension 3
 boundary p p p 
 atom_style atomic
-region box block -20 40 0 200 -20 20
+region box block -15 30 0 175 -20 20
 create_box 1 box
 mass 1 12.011
 lattice custom 1.42 a1 3 0 0 a2 0 1.732 0 a3 0 0 20 &
@@ -33,23 +33,33 @@ basis 0 0 0 &
 basis 0.333 0 0 &
 basis 0.5 0.5 0 &
 basis 0.833 0.5 0
-variable x_0 equal 1
-variable x_f equal 20
-variable y_0 equal 0
-variable y_f equal 200
+variable x_0 equal -1
+variable x_f equal 15
+variable y_0 equal ylo
+variable y_f equal yhi
 variable x equal ${x_f}-${x_0}
 variable y equal ${y_f}-${y_0}
+variable fixed1_yf equal ${y_0}+1 # fixed atoms' final y value
+variable fixed2_yi equal ${y_f}-1 # fixed atoms' final y value
 variable thickness equal 3.35
 variable V equal $x*$y*${thickness}
 region graphene block ${x_0} ${x_f} ${y_0} ${y_f} -0.1 0.1 units box ## does z coordinates appropriate?
 create_atoms 1 region graphene
+
+region fixed_atoms1 block ${x_0} ${x_f} ${y_0} ${fixed1_yf} -0.1 0.1 units box 
+region fixed_atoms2 block ${x_0} ${x_f} ${fixed2_yi} ${y_f} -0.1 0.1 units box 
+group fixed_atoms1 region fixed_atoms1
+group fixed_atoms2 region fixed_atoms2
+group fixed_atoms union fixed_atoms1 fixed_atoms2
+#fix freeze fixed_atoms setforce 0.0 0.0 0.0
+
 pair_style airebo 3.0
 pair_coeff * * CH.airebo C
 minimize 1.0e-6 1.0e-8 1000 10000
 timestep ${dt}
 thermo $d
 thermo_style custom step temp pe ke etotal
-dump 1 all custom 10000 nr.dump id type x y z vx vy vz
+dump 1 all custom 10000 nrr.dump id type x y z vx vy vz
 
 
 velocity all create 10.0 48279
@@ -64,7 +74,7 @@ run ${ir}
 undump 1
 
 reset_timestep 0
-dump myDump all atom 10000 dump_reset_nr.lammpstrj
+dump myDump all atom 10000 dump_reset_nr_fixed.lammpstrj
 
 
 compute ke all ke/atom
